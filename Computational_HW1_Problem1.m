102acf211e849b91861390ce7873c3a312669750
jared <jared@DESKTOP-69KJITU>
2025-09-03 22:50:16 -0600

    basic outline

--- Computational_HW1_Problem1.m
+++ Computational_HW1_Problem1.m
@@ -10,7 +10,7 @@ time t. To implement this you need to implement three distinct steps:
 (a) Compute the orbital elements from an arbitrary position vector, 
 velocity vector, and time.
 
-(b) Solve Kepler s equation for an arbitrary time t to find the
+(b) Solve Kepler's equation for an arbitrary time t to find the
 corresponding value of true anomaly.
 
 (c) Using the orbital elements, specify the position and velocity vectors
@@ -27,7 +27,18 @@ Propagate the orbit over two orbital periods using time intervals of
 60 seconds.Plot the resulting trajectory in position and velocity space.
 
 ii. Use the same value of μ as above. Assume an initial radius of periapsis
-distance of 10, 000 km, and orbit element angles i = 135◦, Ω = 45◦, and
+distance of 10,000 km, and orbit element angles i = 135◦, Ω = 45◦, and
 ω = −90◦. For these given orbit elements, compute and plot the orbit in
 x − y − z space over two orbit periods for: e = 0, 0.25, 0.5, 0.75, 0.9
 %}
+
+% ICs
+mu = 4e5; % km^3/s^2
+r_o = [6 6 6] .* 1e3; % km
+v_o = [-5 5 0]; % km/s
+
+
+function[x,y,z] = orbital_elements(mu,r_o,v_o)
+
+end
+
