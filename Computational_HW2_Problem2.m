e975ad5349b04d88fde16c6fc5482fea71cdb075
jared <jared@DESKTOP-69KJITU>
2025-10-27 14:39:41 -0600

    finished problem 2

--- Computational_HW2_Problem2.m
+++ Computational_HW2_Problem2.m
@@ -66,106 +66,148 @@ tspan = t0:t_step:T;
 
 % ode45 call
 options = odeset('RelTol',1e-6,'AbsTol',1e-9);
-[t,state] = ode45(@(tspan,var) OrbitEOM_J2(tspan,var,mu,RE,J2),tspan,var,options);
+[t1,state] = ode45(@(tspan,var) OrbitEOM_J2(tspan,var,mu,RE,J2),tspan,var,options);
 
 % Orbital Elements
-[~,~,~,~,~,~,hz,epsilon] = orbital_elements_J2(mu,RE,J2,state,t);
-
-figure(1);
-subplot(2,1,1)
-plot(t/3600,hz,'LineWidth',2)
-xlabel('Time [hr]')
-ylabel("Angular Momentum about the z-axis $h_z$ [$km^2/s$]",'Interpreter','latex')
-grid on;grid minor
-subplot(2,1,2)
-plot(t/3600,epsilon,'LineWidth',2)
-xlabel('Time [hr]')
-ylabel("Energy $\varepsilon$",'Interpreter','latex')
-grid on;grid minor
-sgtitle('Integrals of Motion')
+[~,~,~,~,~,~,hz,epsilon] = orbital_elements_J2(mu,RE,J2,state,t1);
 
 %% part (b)
 
 % Givens
-a = 7000; % km
-e = 0.1;
-i = deg2rad(45); % rad
-w = 0; % rad
-Omega = 0; % rad
+a_i = 7000; % km
+e_i = 0.1;
+i_i = deg2rad(45); % rad
+w_i = 0; % rad
+Omega_i = 0; % rad
 tp = 0; % rad
 
 % Finding intial r and v (assume at periapsis)
-rp = a*(1-e);
-vp = sqrt(mu/a*((1+e)/(1-e)));
+rp = a_i*(1-e_i);
+vp = sqrt(mu/a_i*((1+e_i)/(1-e_i)));
 
 % Unit vectors
 x_hat = [1 0 0]';
 y_hat = [0 1 0]';
 z_hat = [0 0 1]';
-n_Omega_hat = cos(Omega)*x_hat + sin(Omega)*y_hat;
-n_Omega_hat_perp = -cos(i)*sin(Omega)*x_hat + ...
-    cos(i)*cos(Omega)*y_hat + sin(i)*z_hat;
-e_hat = cos(w)*n_Omega_hat + sin(w)*n_Omega_hat_perp;
-e_hat_perp = -sin(w)*n_Omega_hat + cos(w)*n_Omega_hat_perp;
+n_Omega_hat = cos(Omega_i)*x_hat + sin(Omega_i)*y_hat;
+n_Omega_hat_perp = -cos(i_i)*sin(Omega_i)*x_hat + ...
+    cos(i_i)*cos(Omega_i)*y_hat + sin(i_i)*z_hat;
+e_hat = cos(w_i)*n_Omega_hat + sin(w_i)*n_Omega_hat_perp;
+e_hat_perp = -sin(w_i)*n_Omega_hat + cos(w_i)*n_Omega_hat_perp;
 
 r0 = rp*e_hat;
 v0 = vp*e_hat_perp;
 var = [r0;v0];
 
 % Orbital Period
-n = sqrt(mu/a^3);
+n = sqrt(mu/a_i^3);
 T = (2*pi)/n;
 
 % Time vector
 t_step = 60;
-tspan = tp:t_step:10*T;
+tspan = tp:t_step:5*T;
 
 % ode45 call
 options = odeset('RelTol',1e-6,'AbsTol',1e-9);
 [t,state2] = ode45(@(tspan,var) OrbitEOM_J2(tspan,var,mu,RE,J2),tspan,var,options);
 
 % Orbital Elements
-[a,e,inc,Omega,w,sigma,hz,epsilon] = orbital_elements_J2(mu,RE,J2,state2,t);
+[a,e,inc,Omega,w,sigma,~,~] = orbital_elements_J2(mu,RE,J2,state2,t);
+sigma = unwrap(sigma);
+
+% Calculate 1st Order Averages
+alpha = [a,e,inc,Omega,w,sigma];
+a_bar = a_i*ones(length(a),1);
+e_bar = e_i*ones(length(e),1);
+i_bar = i_i*ones(length(inc),1);
+[w_bar_d,O_bar_d,s_bar_d] = FirstOrderAveragedLPEs_J2(alpha,mu,RE,J2);
+
+%% part (c)
+% Average from numerical integration
+a_num_bar = mean(a)*ones(length(a));
+e_num_bar = mean(e)*ones(length(e));
+i_num_bar = mean(inc)*ones(length(inc));
+
+% Calculate 1st Order Averages
+alpha2 = [a_num_bar,e_num_bar,i_num_bar,Omega,w,sigma];
+[w_bar_d2,O_bar_d2,s_bar_d2] = FirstOrderAveragedLPEs_J2(alpha2,mu,RE,J2);
+
+
+%% Plots 
+figure(1);
+subplot(2,1,1)
+plot(t1/3600,hz,'LineWidth',2)
+xlabel('Time [hr]')
+ylabel("Angular Momentum about the z-axis $h_z$ [$km^2/s$]",'Interpreter','latex')
+grid on;grid minor
+subplot(2,1,2)
+plot(t1/3600,epsilon,'LineWidth',2)
+xlabel('Time [hr]')
+ylabel("Energy $\varepsilon$",'Interpreter','latex')
+grid on;grid minor
+sgtitle('Integrals of Motion')
 
 figure(2);
 subplot(2,1,1)
 plot(t/3600,a,'LineWidth',2)
+hold on
+plot(t/3600,a_bar,'LineWidth',2)
+plot(t/3600,a_num_bar,'g','LineWidth',2)
 xlabel('Time [hr]')
 ylabel("Semi-major axis $a$ [km]",'Interpreter','latex')
 grid on;grid minor
+hold off
 subplot(2,1,2)
 plot(t/3600,e,'LineWidth',2)
+hold on
+plot(t/3600,e_bar,'LineWidth',2)
+plot(t/3600,e_num_bar,'g','LineWidth',2)
 xlabel('Time [hr]')
 ylabel("Eccetricity $e$",'Interpreter','latex')
 grid on;grid minor
 sgtitle('Orbital Elements: Geometry')
+legend('Numerical Integration','Secular (No Short Period Corrections)','Secular with Short Period Corrections')
 
 figure(3);
 subplot(3,1,1)
 plot(t/3600,rad2deg(Omega),'LineWidth',2)
+hold on
+plot(t/3600,rad2deg(O_bar_d*t),'LineWidth',2)
+plot(t/3600,rad2deg(O_bar_d2*t),'g','LineWidth',2)
 xlabel('Time [hr]')
 ylabel("$\Omega$ [degrees]",'Interpreter','latex')
 grid on;grid minor
+hold off
 subplot(3,1,2)
 plot(t/3600,rad2deg(w),'LineWidth',2)
+hold on
+plot(t/3600,rad2deg(w_bar_d*t),'LineWidth',2)
+plot(t/3600,rad2deg(w_bar_d2*t),'g','LineWidth',2)
 ylabel("$\omega$ [degrees]",'Interpreter','latex')
 grid on;grid minor
+hold off
 subplot(3,1,3)
-plot(t/3600, rad2deg(inc),'LineWidth',2)
+plot(t/3600,rad2deg(inc),'LineWidth',2)
+hold on
+plot(t/3600,rad2deg(i_bar),'LineWidth',2)
+plot(t/3600,rad2deg(i_num_bar),'g','LineWidth',2)
 ylabel("$i$ [degrees]",'Interpreter','latex')
 grid on;grid minor
 sgtitle('Orbital Elements: Angles')
+legend('Numerical Integration','Secular (No Short Period Corrections)','Secular with Short Period Corrections')
 
 figure(4);
-plot(t/3600,sigma,'LineWidth',2)
+plot(t/3600,rad2deg(sigma),'LineWidth',2)
+hold on
+plot(t/3600,rad2deg(s_bar_d*t),'LineWidth',2)
+plot(t/3600,rad2deg(s_bar_d2*t),'g','LineWidth',2)
 grid on; grid minor
 xlabel('Time [hr]')
 ylabel('$\sigma$ [degrees]','Interpreter','latex')
 title('Orbital Elements: $\sigma = -nt_p$','Interpreter','latex')
+legend('Numerical Integration','Secular (No Short Period Corrections)','Secular with Short Period Corrections')
 
 
-
-%% part (c)
 
 %% Functions
 function [var_dot] = OrbitEOM_J2(~,var,mu,RE,J2)
@@ -259,4 +301,21 @@ function [a,e,inc,Omega,w,sigma,hz_vec,epsilon] = orbital_elements_J2(mu,RE,J2,s
         tp = t(j) - (1/n)*(E - e(j)*sin(E));
         sigma(j) = -n*tp;
     end
+end
+
+function [w_bar_d,O_bar_d,s_bar_d] = FirstOrderAveragedLPEs_J2(alpha,mu,RE,J2)
+    % Goal: generate 1st order averaged LPEs for J2 pertubation
+
+    % Break apart alpha
+    a = alpha(:,1);
+    e = alpha(:,2);
+    i = alpha(:,3);
+
+    % Calculate 1st order averages
+    for j = 1:length(a) 
+        w_bar_d = 3*sqrt(mu/a(j)^3)*((RE^2*J2)/(a(j)^2*(1-e(j)^2)^2))*(1-1.25*(sin(i(j)))^2);
+        O_bar_d = -1.5*sqrt(mu/a(j)^3)*((RE^2*J2)/(a(j)^2*(1-e(j)^2)^2))*cos(i(j));
+        s_bar_d = 1.5*sqrt(mu/a(j)^3)*((RE^2*J2)/(a(j)^2*(1-e(j)^2)^2))*sqrt(1-e(j)^2)*(1-1.5*(sin(i(j)))^2);
+    end
+
 end
